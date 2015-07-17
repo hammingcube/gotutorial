a1d080762743871a3801cf888c83c430d01413fc
maddyonline <madhavjha@gmail.com>
2015-07-17 03:15:54 -0700

    minor

--- main.go
+++ main.go
@@ -88,6 +88,6 @@ func main() {
 	http.Handle("/static/", http.FileServer(http.Dir(*baseFlag)))
 	http.Handle("/socket", socket.NewHandler(origin))
 	http.Handle("/", srv)
-	log.Println("Starting the server...")
+	log.Println("Starting the blog server...")
 	log.Fatal(http.Serve(ln, nil))
 }
